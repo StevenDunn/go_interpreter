a90073964ea0f253499731e3aab0bfcbeb3698e5
Steven Dunn <stevendunn@gmail.com>
2019-02-16 16:26:05 +0000

    adding return statements to parser

--- parser/parser.go
+++ parser/parser.go
@@ -54,10 +54,12 @@ func (p *Parser) ParseProgram() *ast.Program {
 	return program
 }
 
-func (p *Parse) parseStatement() ast.Statement {
+func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -100,4 +102,17 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.peekError(t)
 		return false
 	}
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: we're skipping the expressions until we encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
 }
