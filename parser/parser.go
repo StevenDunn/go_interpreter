64d4bcba89ae707480dd575b6ac789119721f448
Steven Dunn <stevendunn@gmail.com>
2019-02-20 12:44:37 +0000

    adding grouped expressions

--- parser/parser.go
+++ parser/parser.go
@@ -41,6 +41,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+
 	// read two tokens to set both vars
 	p.nextToken()
 	p.nextToken()
@@ -264,4 +266,16 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
 }
