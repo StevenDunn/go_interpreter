139e189448b83c3faa7bff4dfc1632ff03b6cb20
Steven Dunn <stevendunn@gmail.com>
2019-02-21 03:03:05 +0000

    updating REPL to use evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/StevenDunn/go_interpreter/evaluator"
 	"github.com/StevenDunn/go_interpreter/lexer"
 	"github.com/StevenDunn/go_interpreter/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
